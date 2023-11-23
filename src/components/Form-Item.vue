ef624176021b3c98ea1e618c921f8e8e441c71b7
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-11-23 17:18:07 +0100

    npm run format

--- src/components/Form-Item.vue
+++ src/components/Form-Item.vue
@@ -1,13 +1,13 @@
 <script>
 export default {
-  name: 'FormItem'
+  name: 'FormItem',
 }
 </script>
 <template>
   <form class="container">
     <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Nom & Prénom</label>
-      <input type="text" class="form-control" id="exampleInputEmail1"/>
+      <input type="text" class="form-control" id="exampleInputEmail1" />
     </div>
     <div class="mb-3">
       <label for="citation" class="form-label">Citation</label>
@@ -17,7 +17,9 @@ export default {
         id="citation"
         aria-describedby="citationHelp"
       ></textarea>
-      <div id="citationHelp" class="form-text fst-italic">Un petit résumé du cours que vous avez suivi</div>
+      <div id="citationHelp" class="form-text fst-italic">
+        Un petit résumé du cours que vous avez suivi
+      </div>
     </div>
     <div class="mb-3 form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1" />
