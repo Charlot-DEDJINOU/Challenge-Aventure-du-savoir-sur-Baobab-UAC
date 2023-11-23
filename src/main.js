d63570716f4908e6df5427ea1612c685f8ac9de5
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-11-23 20:26:09 +0100

    npm run format

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(store)
 
 app.mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-   image : "Aucun fichier n'est choisi"
+    image: "Aucun fichier n'est choisi"
   },
   getters: {},
   mutations: {
     setImage(state, image) {
       state.image = image
-    },
+    }
   },
   actions: {
     ChangeImage({ commit }, image) {
